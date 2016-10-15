b6cb5e3d2825535fd6aac6f392778db1f1bb8ff4
Hasya Shah <benoxgfx@gmail.com>
2016-10-15 18:55:21 -0400

    Added 4 buttons to the controller and added base event listeners

--- app/user/index.js
+++ app/user/index.js
@@ -4,4 +4,13 @@
 
     // const roomId = window.location.pathname.split('/')[1].toLowerCase();
     // const socket = io.connect(`/${roomId}`);
+
+    let buttons = document.querySelectorAll('.button');
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(ev) {
+            let button = ev.target;
+            while (button.className !== 'button') button = button.parentNode;
+            console.log(button.dataset.ring);
+        });
+    });
 })();
