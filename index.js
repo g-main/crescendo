5786fadc9015179f3acdd3150e671254f44c05bd
Hasya Shah <benoxgfx@gmail.com>
2016-10-15 17:48:34 -0400

    Stripped out SpotlightBling backend code and removed Redis

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const
-    debug = require('debug')('spotlightbling:index'),
+    debug = require('debug')('crescendo:index'),
     express = require('express'),
     http = require('http'),
     path = require('path'),
 
     app = express(),
     httpServer = http.createServer(app),
+    apiRouter = express.Router(),
 
     io = require('./server/socket'),
     room = require('./server/room');
@@ -14,39 +15,24 @@ app.use('/assets', express.static('./dist/assets'));
 app.use('/js', express.static('./dist/js'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './app/landing/index.html'));
+    res.sendFile(path.join(__dirname, './dist/host.html'));
 });
 
 app.get(/^\/[a-fA-F0-9]{4}\/?$/, function(req, res) {
-    room.checkRoomExists(req.url.split('/')[1].toLowerCase(), function(err, exists) {
-        if (err || !exists) {
-            res.redirect('/');
-        } else {
-            res.sendFile(path.join(__dirname, './app/user/index.html'));
-        }
-    });
+    if (room.checkRoomExists(req.url.split('/')[1].toLowerCase())) {
+        res.sendFile(path.join(__dirname, './dist/user.html'));
+    } else {
+        res.redirect('/');
+    }
 });
 
-app.get(/^\/[a-fA-F0-9]{16}\/?$/, function(req, res) {
-    room.checkHostExists(req.url.split('/')[1].toLowerCase(), function(err, exists) {
-        if (err || !exists) {
-            res.redirect('/');
-        } else {
-            res.sendFile(path.join(__dirname, './app/host/index.html'));
-        }
-    });
+apiRouter.get('/create', function(req, res) {
+    let host = room.createHost();
+    io.createNamespace(host.roomId);
+    res.json(host);
 });
 
-app.get('/create', function(req, res) {
-    room.createRoom(function(err, r) {
-        if (err || !r) {
-            res.send(500);
-        } else {
-            io.createNamespace(r.roomId);
-            res.redirect(`/${r.hostId}`);
-        }
-    });
-});
+app.use('/api/v0', apiRouter);
 
 httpServer.listen(3000, function() {
     debug('OMG! Server is up!');
--- server/socket.js
+++ server/socket.js
@@ -1,7 +1,6 @@
 const
-    debug = require('debug')('spotlightbling:socket'),
-    socketio = require('socket.io'),
-    room = require('./room');
+    debug = require('debug')('crescendo:socket'),
+    socketio = require('socket.io');
 
 let io;
 
@@ -45,33 +44,5 @@ function createNamespace(roomId) {
                 debug('Uhh... why did leaveSection not include a section?');
             }
         });
-
-        // This event should only be sent from the host.
-        socket.on('setColor', function(data) {
-            if (data && data.roomId && data.hostId && data.section && data.color) {
-                room.checkHostExists(data.hostId, function(err, exists) {
-                    if (err || !exists) {
-                        debug('You dun messed up A-A-RON.');
-                    } else {
-                        socket.to(data.section).emit('changeColor', { color: data.color });
-                    }
-                });
-            } else {
-                debug('Bruh... setColor requires roomId, hostId, section, and color.')
-            }
-        });
     });
-
-    // startSlideshow(roomId, 'sec1');
-}
-
-function startSlideshow(roomId, section) {
-    let colors = ['#E53400', '#1DEFFF', '#65E538', '#FFC937', '#9200E5'],
-        idx = 0;
-
-    setInterval(function() {
-        debug('Emitting ' + colors[idx] + ' to ' + section + ' in room: ' + roomId);
-        io.of('/' + roomId).to(section).emit('changeColor', { color: colors[idx++] });
-        if (idx === colors.length) idx = 0;
-    }, 3000);
 }
