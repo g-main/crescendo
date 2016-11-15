9b6adc164ed1fccc5a34a56d2e94939615fdf6cd
Geoffrey Yu <geoffxy@gmail.com>
2016-11-14 19:00:56 -0500

    Switch to Lato

--- app/host/TextStyles.js
+++ app/host/TextStyles.js
@@ -1,15 +1,15 @@
 (function(){
     module.exports = {
         TITLE_FONT_STYLE: {
-            font: '100px Arial',
+            font: '100px Lato',
             fill: '#ffffff'
         },
         TEXT_FONT_STYLE: {
-            font: '40px Arial',
+            font: '40px Lato',
             fill: '#ffffff'
         },
         CALL_TO_ACTION_FONT_STYLE: {
-            font: '50px Arial',
+            font: '50px Lato',
             fill: '#ffffff'
         }
     };
