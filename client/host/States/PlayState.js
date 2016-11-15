c50415eb1d374daef3e108d1f40c8f9ae451bff1
rageandqq <chitley.sameer@gmail.com>
2016-11-15 17:13:13 -0500

    Dead code cleanup

--- client/host/States/PlayState.js
+++ client/host/States/PlayState.js
@@ -12,7 +12,6 @@ const TRACK_NOTES = [
     [1500, 2000, 9500, 10000, 12000, 18750, 20750, 25500, 26000],
 ];
 
-// const NUM_NOTES = TRACK_NOTES.reduce((prev, curr) => prev + curr.length, 0);
 const NOTE_DELTA_Y = 5;
 
 const NOTE_SIZE = {
@@ -29,9 +28,6 @@ const TRACK_KEY = 'track';
 // The amount of time (in note travel distance) after which the
 // last note moves offscreen before we move to the next state
 const END_GAME_OFFSET = 240;
-
-// The size of the note inset (how big the "colored" part of the ring should be)
-// const NOTE_INSET = 9;
 
 class Note {
     constructor(graphics, trackNum, time) {
@@ -93,7 +89,7 @@ export default class PlayState extends GameState {
                 this.player.score += 10;
             }
         });
-        // TODO: delete note on success
+        // TODO: delete note on success (if implemented, need to keep last note)
     }
 
     initializeSocket(socket) {
@@ -214,7 +210,6 @@ export default class PlayState extends GameState {
         const relativeTime = Date.now() - this.startTime;
         for (let i = 0; i < this.notes.length; i++) {
             const note = this.notes[i];
-            // note.incrementY(NOTE_DELTA_Y);
             note.recalculatePosition(relativeTime);
         }
 
