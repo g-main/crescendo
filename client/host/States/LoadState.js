7ca3ff72365f9f8ac0a60993a50a22adf3f1ced5
rageandqq <chitley.sameer@gmail.com>
2016-11-17 21:04:09 -0500

    Added white drums and guitar to game

--- client/host/States/LoadState.js
+++ client/host/States/LoadState.js
@@ -5,6 +5,8 @@ export default class LoadState extends GameState {
     preload() {
         this.game.load.image(INSTRUMENTS.DRUMS, 'assets/img/drums.png');
         this.game.load.image(INSTRUMENTS.GUITAR, 'assets/img/guitar.png');
+        this.game.load.image(`${INSTRUMENTS.DRUMS}_white`, 'assets/img/drums_white.png');
+        this.game.load.image(`${INSTRUMENTS.GUITAR}_white`, 'assets/img/guitar_white.png');
     }
 
     create() {
