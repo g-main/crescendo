36ebdb687d3056d6f8217b81d016f08b92afdcd8
rageandqq <chitley.sameer@gmail.com>
2016-11-18 19:05:05 -0500

    Added difficulty and artist to Song model

--- client/host/Models/Song.js
+++ client/host/Models/Song.js
@@ -4,6 +4,8 @@ export default class Song {
     constructor(song) {
         this._name = song.name;
         this._file = song.file;
+        this._difficulty = song.difficulty;
+        this._artist = song.artist;
         this._tracks = {};
         Object.keys(song.track).forEach(instrument => {
             this._tracks[instrument] = new Track(instrument, song.track[instrument]);
@@ -16,5 +18,9 @@ export default class Song {
 
     getTrack(type) {
         return this._tracks[type];
+    }
+
+    get difficulty() {
+        return this._difficulty;
     }
 }
